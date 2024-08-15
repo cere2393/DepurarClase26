2864d83fbeec60d48745eb6d62df6ecae6e54568
cere2393 <carlos.ramirez@tecnologicoliceocristiano.edu.ec>
2024-08-15 14:52:57 -0500

    Add files via upload

---
+++ 3.ValorMaximoCorregido.py
@@ -0,0 +1,17 @@
+def encontrar_maximo(numeros):
+
+    maximo = numeros[0]
+
+    for numero in numeros:
+
+        if numero > maximo:
+
+            maximo = numero
+
+    return maximo
+
+
+
+lista_numeros = [-10, -20, -30, -5, -15]
+
+print("El valor máximo es:", encontrar_maximo(lista_numeros))
